7c29f8ae0f559dcb0401d45b05c7108a81eccfce
Karl Horky <karl.horky@gmail.com>
2023-11-24 16:39:46 +0100

    Ignore cypress.config.ts

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,14 @@
-export { default } from 'eslint-config-upleveled';
+import upleveled from 'eslint-config-upleveled';
+
+/** @type {import('@typescript-eslint/utils/ts-eslint').FlatConfig.ConfigArray} */
+const config = [
+  ...upleveled,
+  {
+    ignores: [
+      // Unused variables in default Cypress config
+      'cypress.config.ts',
+    ],
+  },
+];
+
+export default config;
